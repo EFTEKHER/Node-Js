443f59a59c7333f96785f5a6bbd96d86322cff65
EFTEKHER <eftekherali2000@gmail.com>
2023-06-15 01:54:02 +0600

    done

---
+++ chapter1/1_hello_world.js
@@ -0,0 +1,10 @@
+console.log(global);
+
+
+const os=require('os');
+console.log(os.type());
+console.log(os.version());
+
+console.log(os.homedir());
+console.log(__dirname);
+console.log(__filename);
