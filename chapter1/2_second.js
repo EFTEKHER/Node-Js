ddf2c8a120af6ccb4ba6559dbd1e2264093b0749
EFTEKHER <eftekherali2000@gmail.com>
2023-06-15 02:10:07 +0600

    done

--- chapter1/2_second.js
+++ chapter1/2_second.js
@@ -1,6 +1,9 @@
 const path= require('path')
-
+console.log(1);
 console.log(path.dirname(__filename));
+console.log(2);
 console.log(path.basename(__filename));
+console.log(3);
 console.log(path.extname(__filename));
+console.log(4);
 console.log(path.parse(__filename));
