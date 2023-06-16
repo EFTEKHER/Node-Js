70a8350e336ddba06a26fb8d4427bea2d7fd8472
EFTEKHER <eftekherali2000@gmail.com>
2023-06-16 22:36:15 +0600

    done

--- chapter2/5_index.js
+++ chapter2/5_index.js
@@ -7,7 +7,7 @@ const fileOps=async()=>{
     try{
         const data=await fsPromises.readFile(path.join(__dirname,'files','starter.txt'),'utf-8');
         console.log(data);
-        await fsPromises.unlink(path.join(__dirname,'starter.txt'));
+        await fsPromises.unlink(path.join(__dirname,'files','starter.txt'));
         await fsPromises.writeFile(path.join(__dirname,'files','promiseWrite.txt'),data);
         await fsPromises.appendFile(path.join(__dirname,'files','promiseWrite.txt'),'\n Nice to meet you');
         await fsPromises.rename(path.join(__dirname,'files','promiseWrite.txt'),path.join(__dirname,'files','promiseComplete.txt'));
